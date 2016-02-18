fd85f165d1ef08cd7df14a6176e99c8cef75dfbc
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-02-18 16:01:11 -0600

    finished for the day

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
+const logger = require('./lib/logger');
 const routes = require('./routes/note');
 
 const PORT = process.env.PORT || 3000; // eslint-disable-line no-magic-numbers
@@ -14,12 +15,12 @@ app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded( {extended: false} ) );
 app.use(bodyParser.json() );
 app.use(methodOverride('_method'));
-
-app.use(routes);
+app.use(logger);
 
 app.get('/', (req, res) => {
   res.send('Server Is Go!');
 });
+app.use(routes);
 
 mongoose.connect('mongodb://localhost:27017/forget-me-node', (err) => {
   if (err) throw err;
