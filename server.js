9f35eb9fd485b0e161ce53ecc920d39f866144c7
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-02-18 10:02:42 -0600

    added mongoose and body parser

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 "use strict";
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000; // eslint-disable-line no-magic-numbers
+
+app.use(bodyParser.urlencoded( {extended: false} ) );
+app.use(bodyParser.json() );
+
 app.get('/', (req, res) => {
   res.send('Server Is Go!');
 });
