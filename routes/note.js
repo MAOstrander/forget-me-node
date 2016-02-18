d1a4ceeea1c443d122b30cf97962481ba458c37c
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-02-18 15:36:37 -0600

    refactored a bit to make things look cleaner

--- routes/note.js
+++ routes/note.js
@@ -2,19 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const note = require('../controllers/note');
+const Note = require('../models/note');
+
+router.param('id', (req, res, next, id) => {
+  Note.findById(id, (err, note) => {
+    if (err) throw err;
+
+    req.note = note;
+    next();
+  });
+});
 
 router.get('/notes', note.index);
-
 router.get('/notes/new', note.newNote);
-
 router.get('/notes/:id', note.show);
-
 router.delete('/notes/:id', note.destroy);
-
 router.get('/notes/:id/edit', note.edit);
-
 router.put('/notes/:id', note.update);
-
 router.post('/notes', note.create);
 
 
