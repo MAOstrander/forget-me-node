6cbe462fa43756094f294ba272ef3d0f8483677a
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-02-18 10:53:23 -0600

    note is now saved to local database

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000; // eslint-disable-line no-magic-numbers
+const Note = mongoose.model('Notes', mongoose.Schema({
+  title: String,
+  content: String
+}));
 
 app.use(bodyParser.urlencoded( {extended: false} ) );
 app.use(bodyParser.json() );
@@ -18,11 +22,19 @@ app.get('/', (req, res) => {
 app.get('/notes/new', (req, res) => {
   res.render('new-note');
 });
+
 app.post('/notes', (req, res) => {
-  console.log("Is the form there?", req.body);
+  Note.create(req.body, (err, note) => {
+    if (err) throw err;
+    console.log(note);
+  })
   res.redirect('/');
 });
 
-app.listen(PORT, () => {
-  console.log(`Node.js server started. Listening on port ${PORT}`);
+mongoose.connect('mongodb://localhost:27017/forget-me-node', (err) => {
+  if (err) throw err;
+
+  app.listen(PORT, () => {
+    console.log(`Node.js server started. Listening on port ${PORT}`);
+  });
 });
